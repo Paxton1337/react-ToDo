dedcb8f0593c0942dc876c907779e7d694320bce
Gleb <azrieldevpoper@gmail.com>
2020-06-29 14:55:38 +0300

    Updates

---
+++ precache-manifest.be83ad27bcbe46ac87d8746c077663da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203938fa7a1fa7267aaf39d52d86e25d",
+    "url": "/react-learning/index.html"
+  },
+  {
+    "revision": "3aa33479579e1c3af903",
+    "url": "/react-learning/static/css/main.40427995.chunk.css"
+  },
+  {
+    "revision": "752a2039f4e14ae0030a",
+    "url": "/react-learning/static/js/2.b1f401cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-learning/static/js/2.b1f401cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa33479579e1c3af903",
+    "url": "/react-learning/static/js/main.97d78813.chunk.js"
+  },
+  {
+    "revision": "4e344dafd80d29a27581",
+    "url": "/react-learning/static/js/runtime-main.9e6f19ba.js"
+  }
+]);
